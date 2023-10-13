bad694100c9b8a3c2d3b13160f2260d881c22236
Kartbllansh <movolovo93@gmail.com>
2023-10-13 22:04:15 +0300

    0003 - maven project

---
+++ DataBase/src/test/java/com/shaverma/database/DataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaverma.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
